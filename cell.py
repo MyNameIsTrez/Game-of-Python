d41c5cc360b3deb0b3d8722788aa76eca09895f7
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2019-10-07 23:02:15 +0200

    Finished the basics of Game of Python!

--- cell.py
+++ cell.py
@@ -1,10 +1,9 @@
 import pygame
-
 import random
 
 
-def tuple_add(tup1, tup2):
-    return tuple(sum(x) for x in zip(tup1, tup2))
+# def tuple_add(tup1, tup2):
+#     return tuple(sum(x) for x in zip(tup1, tup2))
 
 
 def tuple_mult(tup, val):
@@ -13,27 +12,66 @@ def tuple_mult(tup, val):
 
 black_cell_img = pygame.image.load("images/black_cell.png")
 white_cell_img = pygame.image.load("images/white_cell.png")
-# cell_img = pygame.image.load("intro_ball.gif")  # load the texture of the cell
 
 # get the image width and height as a tuple
 cell_rect = white_cell_img.get_rect()
 
 
 class Cell:
-    def __init__(self, row, col, cell_size, screen):
-        self.set_pos(row * cell_size, col * cell_size)
+    def __init__(self, col, row, cell_size, screen):
+        self.pos = (col, row)
         self.screen = screen
         self.cell_size = cell_size
-        self.alive = random.randint(0, 1)
-
-    def set_pos(self, x, y):
-        self.pos = (x, y)
+        # self.alive = random.randint(0, 10) < 3
+        if (
+            col == 0 and row == 1 or
+            col == 1 and row == 2 or
+            col == 2 and row == 2 or
+            col == 2 and row == 1 or
+            col == 2 and row == 0
+        ):
+            self.alive = True
+        else:
+            self.alive = False
 
     def draw(self):
         # draw from the top-left corner of this cell
         if self.alive:
-            # self.screen.blit(black_cell_img, cell_rect.move(self.pos))
-            self.screen.blit(black_cell_img, self.pos)
+            self.screen.blit(white_cell_img, tuple_mult(
+                self.pos, self.cell_size))
         else:
-            # self.screen.blit(white_cell_img, cell_rect.move(self.pos))
-            self.screen.blit(white_cell_img, self.pos)
+            self.screen.blit(black_cell_img, tuple_mult(
+                self.pos, self.cell_size))
+
+    def count_neighbors(self, grid_cells, grid_size):
+        top_edge = self.pos[1] == 0
+        bottom_edge = self.pos[1] == grid_size[1] - 1
+        left_edge = self.pos[0] == 0
+        right_edge = self.pos[0] == grid_size[0] - 1
+
+        self.neighbors = 0
+
+        # top-left
+        if (not top_edge and not left_edge):
+            self.neighbors += grid_cells[self.pos[0]-1][self.pos[1]-1].alive
+        # top
+        if (not top_edge):
+            self.neighbors += grid_cells[self.pos[0]][self.pos[1]-1].alive
+        # top-right
+        if (not top_edge and not right_edge):
+            self.neighbors += grid_cells[self.pos[0]+1][self.pos[1]-1].alive
+        # left
+        if (not left_edge):
+            self.neighbors += grid_cells[self.pos[0]-1][self.pos[1]].alive
+        # right
+        if (not right_edge):
+            self.neighbors += grid_cells[self.pos[0]+1][self.pos[1]].alive
+        # bottom-left
+        if (not bottom_edge and not left_edge):
+            self.neighbors += grid_cells[self.pos[0]-1][self.pos[1]+1].alive
+        # bottom
+        if (not bottom_edge):
+            self.neighbors += grid_cells[self.pos[0]][self.pos[1]+1].alive
+        # bottom-right
+        if (not bottom_edge and not right_edge):
+            self.neighbors += grid_cells[self.pos[0]+1][self.pos[1]+1].alive
--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-import sys
-
-import pygame
 import pygame.freetype
-
+import sys
 from grid import Grid
+import pygame
+import time
 
 
 def setup():
     pygame.init()
-    cols = 15
+
+    cols = 10
     rows = 15
     cell_size = 64  # the width and height of the cells' img
+
     size = (cols * cell_size, rows * cell_size)
     screen = pygame.display.set_mode(size)
     font = pygame.freetype.SysFont("comicsansms", 17)
     grid = Grid(cols, rows, cell_size, screen)  # create the grid
+
     return size, screen, font, grid
 
 
@@ -24,18 +26,14 @@ def main():
             if (event.type == pygame.QUIT):
                 sys.exit()
 
-        screen.fill((0, 0, 0))  # fill screen black
+        screen.fill((0, 0, 0))  # fill the screen black
 
-        grid.draw_cells()
-
-        # font.render_to(  # display text
-        #     screen,
-        #     (30, 30),
-        #     "Sample text",
-        #     (255, 255, 255)
-        # )
+        grid.calc_cells_neighbors()
+        grid.draw_cells(screen, font)
+        grid.update_cells()
 
         pygame.display.flip()  # display this frame
+        time.sleep(0.2)
 
 
 size, screen, font, grid = setup()
